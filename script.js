3c69f3fd56af4fb1ef0a18bb3863ea8495fc5f52
Jungmin-Park <46778306+Jungmin-Park@users.noreply.github.com>
2019-01-24 12:21:54 +0900

    버전확인 링크 추가
    신규 버전 이 있을때 링크 노출

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 chrome.runtime.onMessage.addListener(function(request,sender,response) {
   document.querySelector("#_result").innerHTML = request.message;
 });
@@ -6,19 +5,29 @@ chrome.runtime.onMessage.addListener(function(request,sender,response) {
 document.addEventListener('DOMContentLoaded', function() {
   var y = document.getElementById("git_link");
   y.addEventListener("click", openIndex);
+  
+  var x = document.getElementById("reload_extension");
+  x.addEventListener("click", reloadExtension);
+
 })
 
 function openIndex() {
   chrome.tabs.create({active: true, url: "https://github.com/Jungmin-Park/WZ_WTC/releases"});
 }
 
+function reloadExtension() {
+  chrome.runtime.reload(); 
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
+  
 //$(document).ready(function(){
   //버전 읽어오기
   var manifestData = chrome.runtime.getManifest();
   var _version = manifestData.version
   document.querySelector("#version").innerText = _version;
-  
+   
   //신규 버전 확인
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.github.com/repos/jungmin-park/wz_wtc/releases", true);
@@ -27,7 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
       var rtnJson = JSON.parse(xhr.responseText);
       //버전이 다르면 git 링크 노출
       if(_version != rtnJson[0].tag_name){
-        $("#git_link").css("visibility","visible");
+        $("#check_ver").css("visibility","visible");
+        
       }
     }
   }
