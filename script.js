0a391d36d176b199576e58063d364b0245ffdf5b
Jungmin-Park <46778306+Jungmin-Park@users.noreply.github.com>
2019-01-24 11:24:14 +0900

    Add files via upload
    새 버전 확인 링크 추가

--- script.js
+++ script.js
@@ -3,8 +3,36 @@ chrome.runtime.onMessage.addListener(function(request,sender,response) {
   document.querySelector("#_result").innerHTML = request.message;
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var y = document.getElementById("git_link");
+  y.addEventListener("click", openIndex);
+})
+
+function openIndex() {
+  chrome.tabs.create({active: true, url: "https://github.com/Jungmin-Park/WZ_WTC/releases"});
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('main script loaded')
+//$(document).ready(function(){
+  //버전 읽어오기
+  var manifestData = chrome.runtime.getManifest();
+  var _version = manifestData.version
+  document.querySelector("#version").innerText = _version;
+  
+  //신규 버전 확인
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/repos/jungmin-park/wz_wtc/releases", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var rtnJson = JSON.parse(xhr.responseText);
+      //버전이 다르면 git 링크 노출
+      if(_version != rtnJson[0].tag_name){
+        $("#git_link").css("visibility","visible");
+      }
+    }
+  }
+  xhr.send();
+
   chrome.tabs.executeScript({
     file: "content-script.js",
     allFrames: true
