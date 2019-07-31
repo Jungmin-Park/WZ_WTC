0f259e899af6f6783f2ea1cbf92f33c51e1d9efb
Jungmin-Park <46778306+Jungmin-Park@users.noreply.github.com>
2019-07-31 18:38:44 +0900

    Add files via upload
    - 버그 수정

--- content-script.js
+++ content-script.js
@@ -9,180 +9,136 @@ function convertHourFormatToMin(hourFormat){
 }
 
 var message;
-    /*당월 소정근무일수 */
-    str_totWorkDay =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(4)").innerText.replace("일","");
+/*당월 소정근무일수 */
+str_totWorkDay =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(4)").innerText.replace("일","");
 
-    /*당월 누적 근무일*/
-    records = document.querySelectorAll("#grid > div.k-grid-content-locked > table > tbody > tr");
-    if(records.length != 0){
-    var today = new Date();
-    var dd = today.getDate();
-    var workedDay = 0;
-	var workedToday = 0;
-	var advanceWorkDay = 0;	//사전 인정 근무일(휴가 등)
-	var totWorkDay = 0;
-	
-	var lastDate; //기준일
-	var formatted_today = (today.getMonth()+1).toString().padStart(2, "0")+"월 "+ today.getDate().toString().padStart(2, "0")+"일";
-	var todayComeTime;
-	var currentTime = today.getHours()+":"+today.getMinutes();
-	var todayWorkTime= 0 ; //오늘 근무시간
-	var overWorkTime = 0;	//초과근무시간
-	var overWorkTimeToday = 0; //초과 근무시간 오늘까지
-	var basedDay = "어제";	// 기준일
-	var countErrorDay = "";
-	for (i=0; i < 35 ; i++){
-		record = records[i];
-		if(typeof(record) != 'undefined'){
-            datas = record.querySelectorAll("td");
-            
-			if(datas[0].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") == "계"){
-				break;
-			}
-            if(datas[3].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
-				if( i < dd){
-					workedDay++;
-				}else{
-					advanceWorkDay++;
-				}
+/*당월 누적 근무일*/
+records = document.querySelectorAll("#grid > div.k-grid-content-locked > table > tbody > tr");
+if(records.length != 0){
+var today = new Date();
+var dd = today.getDate();
+var workedDay = 0;
+var workedToday = 0;
+var advanceWorkDay = 0;	//사전 인정 근무일(휴가 등)
+var totWorkDay = 0;
 
-				lastDate = datas[0].innerText.split("(")[0] ;
-				datas[0].innerText  = datas[0].innerText.split("(")[0] + "("+ (workedDay+advanceWorkDay)+")";
-                datas[0].style.color  = "blue";
-                datas[0].style.fontWeight = "bold";
-                datas[0].style.fontSize  = "10px";
-				
-            }else{
-				if(datas[2].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
-					if( i < dd-1){
-						datas[0].innerText  = datas[0].innerText.split("(")[0] + "(!)";
-						datas[0].style.color  = "red";
-						datas[0].style.fontWeight = "bold";
-						datas[0].style.fontSize  = "10px";
-						if (countErrorDay != "") 
-							countErrorDay += ", ";
-						countErrorDay += "["+datas[0].innerText.split("(")[0]+"]";
-					}
-				}
-				else
-				{
-				datas[0].style.color  = "inherit";
-				datas[0].style.fontWeight = "normal";
-				datas[0].style.fontSize  = "inherit";
-				}
+var lastDate; //기준일
+var formatted_today = (today.getMonth()+1).toString().padStart(2, "0")+"월 "+ today.getDate().toString().padStart(2, "0")+"일";
+var todayComeTime;
+var currentTime = today.getHours()+":"+today.getMinutes();
+var todayWorkTime= 0 ; //오늘 근무시간
+var overWorkTime = 0;	//초과근무시간
+var overWorkTimeToday = 0; //초과 근무시간 오늘까지
+var basedDay = "어제";	// 기준일
 
-            }
-			
-			//if(datas[0].innerText == formatted_today && datas[9].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
-			if(datas[0].innerText == formatted_today){
-				if(datas[9].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
-					todayComeTime = datas[9].innerText;
-					todayWorkTime = convertHourFormatToMin(currentTime)- convertHourFormatToMin(todayComeTime)
-					//점심시간(13시~14시)이 지난경우 60분 차감
-					if(convertHourFormatToMin(currentTime) > 14*60){
-						todayWorkTime = todayWorkTime - 60;
-					}
-				}
-				if(datas[3].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") == "")
-					workedToday++;
-				else
-					basedDay = "오늘";
-			}
-		}
-		else{
+for (i=0; i < records.length ; i++){
+	record = records[i];
+	if(typeof(record) != 'undefined'){
+		datas = record.querySelectorAll("td");
+		
+		if(datas[0].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") == "계"){
 			break;
 		}
+		if(datas[3].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
+			if( i < dd){
+				workedDay++;
+			}else{
+				advanceWorkDay++;
+			}
+
+			lastDate = datas[0].innerText.split("(")[0] ;
+			datas[0].innerText  = datas[0].innerText.split("(")[0] + "("+ (workedDay+advanceWorkDay)+")";
+			datas[0].style.color  = "blue";
+			datas[0].style.fontWeight = "bold";
+			datas[0].style.fontSize  = "10px";
+			
+		}else{
+			datas[0].style.color  = "inherit";
+			datas[0].style.fontWeight = "normal";
+			datas[0].style.fontSize  = "inherit";
+		}
+		
+		//if(datas[0].innerText == formatted_today && datas[9].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
+		if(datas[0].innerText == formatted_today){
+			if(datas[9].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") != ""){
+				todayComeTime = datas[9].innerText;
+				todayWorkTime = convertHourFormatToMin(currentTime)- convertHourFormatToMin(todayComeTime)
+				//점심시간(13시~14시)이 지난경우 60분 차감
+				if(convertHourFormatToMin(currentTime) > 14*60){
+					todayWorkTime = todayWorkTime - 60;
+				}
+			}
+			if(datas[3].innerText.replace(/(\r\n\t|\n|\r\t)/gm, "").replace(" ", "") == "")
+				workedToday++;
+			else
+				basedDay = "오늘";
+		}
 	}
-	
-	totWorkDay = workedDay+advanceWorkDay;
-    
-	/*당월 잔여 근무일수 */
-    str_remainWorkDay =  str_totWorkDay - (totWorkDay);
-
-    /*당월 현재진행시간(누적근무시간)*/
-    str_sumWorkTime =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(6)").innerText;
-    convertedWorkTime = convertHourFormatToMin(str_sumWorkTime);
-
-    /*당월 근무가능시간 */
-    str_reqWorkTime =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(7)").innerText;
-    convertedreqWorkTime = convertHourFormatToMin(str_reqWorkTime);
-
-    /*누적 일 평균 근무시간*/
-    if(convertedWorkTime==0)
-        avrWorkTime = 0;
-    else
-        avrWorkTime = Math.round(convertedWorkTime/(totWorkDay));
-
-    /*잔여 일 평균 근무시간*/
-    avrRemWorkTime = Math.round(convertedreqWorkTime/str_remainWorkDay);
-
-    /*초과 근무시간*/
-	overWorkTime = convertedWorkTime - ((totWorkDay) * 480);
-	overWorkTimeToday = (convertedWorkTime + todayWorkTime) - ((totWorkDay + workedToday) * 480)
-	/*			
-    message = "<ul>"+
-				"<li>기준일 : "+lastDate+"</li>"+
-				"<li>당월 누적 근무일 : "+workedDay+"일";
-	if(advanceWorkDay >0){
-		message += "("+advanceWorkDay+"일)";
+	else{
+		break;
 	}
-	message += " / "+str_totWorkDay+"일</li>"+
-                "<li>당월 누적 근무시간 : "+str_sumWorkTime+" ("+convertedWorkTime+"분)</li>"+
-                "<li>누적 평균 근무시간 : "+convertMinToHourFormat(avrWorkTime)+" ("+avrWorkTime+"분)</li>"+
-                "<li>필요 평균 근무시간 : "+convertMinToHourFormat(avrRemWorkTime)+" ("+avrRemWorkTime+"분)</li>"+
-				"<li>초과 근무시간 : <font style='color:red'>"+convertMinToHourFormat(overWorkTime)+" ("+overWorkTime+"분)</font></li>"+
-				"</ul>"
-	
-		if(todayComeTime != "" && formatted_today != lastDate ){
-			overWorkTimeToday = (convertedWorkTime + todayWorkTime) - ((totWorkDay+workedToday) * 480)
-			message += "<ul><li>초과 근무시간(오늘까지) : "+convertMinToHourFormat(overWorkTimeToday)+" ("+(overWorkTimeToday)+"분)</li></ul>";
-		}	
-	
-	*/
-	
-	
-	message = "<table id='summary' width='100%'>"+
-				"	<tr>"+
-				"		<th width='50%'>근무인정 시간</th>"+
-				"		<th width='50%'>근무인정 일수</th>"+
-				"	</tr>"+
-				"	<tr>"+
-				"		<td>"+str_sumWorkTime+"</td>"+
-				"		<td>"+workedDay+"일";
-	
-	if(advanceWorkDay >0){
-		message += "("+advanceWorkDay+"일)";
-	}
-	
-	message += 	" / "+str_totWorkDay+"일</td>"+
-				"	</tr>"+
-				"	<tr>"+
-				"		<th>일 평균 근무</th>"+
-				"		<th>잔여 평균 근무</th>"+
-				"	</tr>"+
-				"	<tr>"+
-				"		<td>"+convertMinToHourFormat(avrWorkTime)+"</td>"+
-				"		<td>"+convertMinToHourFormat(avrRemWorkTime)+"</td>"+
-				"	</tr>"+
-				"	<tr>"+
-				"		<th>초과 시간(" + basedDay + "기준)</th>"+
-				"		<th>초과 시간(현재기준)</th>"+
-				"	</tr>"+
-				"	<tr>"+
-				"		<td style='background:"+ (overWorkTime < 0 ? "#FF9999":"#E5FFCC") +"'>"+convertMinToHourFormat(overWorkTime)+"</td>"+
-				"		<td style='background:"+ (overWorkTimeToday < 0 ? "#FF9999":"#E5FFCC") +"'>"+convertMinToHourFormat(overWorkTimeToday)+"</td>"+
-				"	</tr>"+
-				"</table>";
+}
+
+totWorkDay = workedDay+advanceWorkDay;
+
+/*당월 잔여 근무일수 */
+str_remainWorkDay =  str_totWorkDay - (totWorkDay);
+
+/*당월 현재진행시간(누적근무시간)*/
+str_sumWorkTime =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(6)").innerText;
+convertedWorkTime = convertHourFormatToMin(str_sumWorkTime);
+
+/*당월 근무가능시간 */
+str_reqWorkTime =  document.querySelector("#topGrid > div.k-grid-content > table > tbody > tr > td:nth-child(7)").innerText;
+convertedreqWorkTime = convertHourFormatToMin(str_reqWorkTime);
+
+/*누적 일 평균 근무시간*/
+if(convertedWorkTime==0)
+	avrWorkTime = 0;
+else
+	avrWorkTime = Math.round(convertedWorkTime/(totWorkDay));
+
+/*잔여 일 평균 근무시간*/
+avrRemWorkTime = Math.round(convertedreqWorkTime/str_remainWorkDay);
+
+/*초과 근무시간*/
+overWorkTime = convertedWorkTime - ((totWorkDay) * 480);
+overWorkTimeToday = (convertedWorkTime + todayWorkTime) - ((totWorkDay + workedToday) * 480)
+
+
+message = "<table id='summary' width='100%'>"+
+			"	<tr>"+
+			"		<th width='50%'>총 근무 시간</th>"+
+			"		<th width='50%'>총 근무 일수</th>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<td>"+str_sumWorkTime+"</td>"+
+			"		<td>"+workedDay+"일";
+
+if(advanceWorkDay >0){
+	message += "("+advanceWorkDay+"일)";
+}
+
+message += 	" / "+str_totWorkDay+"일</td>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<th>일 평균 근무</th>"+
+			"		<th>잔여 평균 근무</th>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<td>"+convertMinToHourFormat(avrWorkTime)+"</td>"+
+			"		<td>"+convertMinToHourFormat(avrRemWorkTime)+"</td>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<th>초과 시간(" + basedDay + "기준)</th>"+
+			"		<th>초과 시간(현재기준)</th>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<td style='background:"+ (overWorkTime < 0 ? "#FF9999":"#E5FFCC") +"'>"+convertMinToHourFormat(overWorkTime)+"</td>"+
+			"		<td style='background:"+ (overWorkTimeToday < 0 ? "#FF9999":"#E5FFCC") +"'>"+convertMinToHourFormat(overWorkTimeToday)+"</td>"+
+			"	</tr>"+
+			"</table>";
 				
-	if (countErrorDay != ""){
-		message += 	"<div class='alert alert-danger' style='margin-top:5px'>"+
-					"<strong>!!주의!!</strong><br>"+
-					"<span style='font-weight:500'>"+countErrorDay + "</span>은 근무인정이 되지 않았습니다.<br>"+
-					"세부현황에서 근태 상태를 확인해 주세요"+
-					"</div>";
-        
-	}
-					
-    chrome.runtime.sendMessage({'message': message})
+chrome.runtime.sendMessage({'message': message})
 }
 
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
 chrome.runtime.onMessage.addListener(function(request,sender,response) {
   document.querySelector("#_result").innerHTML = request.message;
 });
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
   var y = document.getElementById("git_link");
@@ -11,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
   
   var x = document.getElementById("reload_extension");
   x.addEventListener("click", reloadExtension);
-
 })
 
 function openIndex() {
@@ -22,31 +18,281 @@ function reloadExtension() {
   chrome.runtime.reload(); 
 }
 
+/* 분을 시:분 Format 문자열로 변경 */
+function convertMinToHourFormat(mins){
+  if (mins ==0)
+		return "0";
+  return (mins < 0 ? "-" : "")+ parseInt(Math.abs(mins) / 60)+ ":" + parseInt(Math.round(Math.abs(mins) % 60)).toString().padStart(2, "0");
+}
+
+/* 시:분 Fortmat 문자열을 분으로 변경 */
+function convertHourFormatToMin(hourFormat){
+  return hourFormat.split(":")[0]*60 + hourFormat.split(":")[1]*1
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-  //버전 읽어오기
-  var manifestData = chrome.runtime.getManifest();
-  var _version = manifestData.version
-  document.querySelector("#version").innerText = _version;
-   
-  //신규 버전 확인
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.github.com/repos/jungmin-park/wz_wtc/releases", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var rtnJson = JSON.parse(xhr.responseText);
-      //버전이 다르면 git 링크 노출
-      if(_version != rtnJson[0].tag_name){
-        $("#check_ver").css("visibility","visible");
-        
+
+  //변수 선언
+  var today = new Date(); 
+  var _year = today.getFullYear(); 
+  var _month = new String(today.getMonth()+1).padStart(2, '0'); 
+  var _day = new String(today.getDate()).padStart(2, '0'); 
+  var _today = _month.toString() + _day.toString();
+  var _compSeq = "";
+  var _deptName = "";
+  var _empSeq = "";
+  
+  var base_work_day ="";  //월 기준근무 일수
+  var sumWorkTime = 0;  // 총 근무시간
+  var v_sumWorkTime =""; // 총 근무시간(시:분포맷)
+  var basedDay = "어제";	// 기준일
+  var workedDay = 0;  //누적 근무일수
+  var advanceWorkDay = 0;	//사전 인정 근무일(휴가 등)
+  
+  var totWorkDay = 0; // 총근무 일수 workedDay + advanceWorkDay
+  var remWorkDay = 0;  //"잔여 근무일수
+  
+  var avrWorkTime =""; // 평균 근무시간
+  var remAvrWorkTime =""; // 잔여 평균 근무시간
+  var overWorkTime = 0;
+  var currentTime = new String(today.getHours()).padStart(2, '0')+":"+new String(today.getMinutes()).padStart(2, '0'); //현재시간
+  var longCurrentTime = new String(today.getHours()).padStart(2, '0')+":"+new String(today.getMinutes()).padStart(2, '0')+":"+new String(today.getSeconds()).padStart(2, '0'); //현재시간
+  var workedToday = 0; //근무일수에 오늘 카운트 여부, 카운트 됐으면 1
+  var todayWorkTime = 0; // 오늘 근무시간, 출근시간 ~ 현재
+  
+  var todayLeftHour = "";
+  
+  checkVersion();   //버전 체크
+  getUserInfo();  //근무시간 계산
+  
+  /* 버전 체크 */
+  function checkVersion(){
+	  
+    //버전 읽어오기
+    var manifestData = chrome.runtime.getManifest();
+    var _version = manifestData.version
+    document.querySelector("#version").innerText = _version;
+    
+    //신규 버전 확인
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/repos/jungmin-park/wz_wtc/releases", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        var rtnJson = JSON.parse(xhr.responseText);
+        //버전이 다르면 git 링크 노출
+        if(_version != rtnJson[0].tag_name){
+          $("#check_ver").css("visibility","visible");
+        }
       }
     }
+    xhr.send();
   }
-  xhr.send();
 
-  chrome.tabs.executeScript({
-    file: "content-script.js",
-    allFrames: true
-    });
-  });
+  /* 정보 조회 */
+  function getUserInfo(){
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (xhr.status === 200 || xhr.status === 201) {
+        var rtnJson = JSON.parse(xhr.responseText);
+
+        for(var i =0 ; i< rtnJson["positionList"].length; i++){
+          //alert(rtnJson["positionList"][i]["deptName"])
+          if(rtnJson["positionList"][i]["mainCompYn"]=="Y" && rtnJson["positionList"][i]["mainDeptYn"]=="Y"){
+            _compSeq = rtnJson["positionList"][i]["compSeq"];
+            _deptName = rtnJson["positionList"][i]["deptName"];
+            _empSeq = rtnJson["positionList"][i]["empSeq"];
+          }
+        }
+        getAttendSummary();
+      } else if (xhr.status === 500) {
+        document.querySelector("#_result").innerHTML = '그룹웨어에 로그인 해 주세요';
+      } else {
+        document.querySelector("#_result").innerHTML = 'Error : '+xhr.responseText;
+      }
+    };
+    xhr.open('GET', 'http://gwa.webzen.co.kr/gw/getPositionList.do');
+    xhr.setRequestHeader('Content-Type', 'application/json'); 
+    xhr.send(); 
+  };
+
+  /* 월별 근태 현황 집계 조회*/
+  function getAttendSummary(){
+    var xhr = new XMLHttpRequest();
+    var data = {
+      compSeq: _compSeq,
+      deptName: _deptName,
+      empSeq: _empSeq,
+      month: _month,
+      year: _year,
+    };
+    xhr.onload = function() {
+      if (xhr.status === 200 || xhr.status === 201) {
+        var rtnJson = JSON.parse(xhr.responseText);
+        v_base_work = rtnJson.attDeptMgrList[0].v_base_work;
+        base_workTime = rtnJson.attDeptMgrList[0].sum_base_work;
+        base_work_day = Number(rtnJson.attDeptMgrList[0].v_base_work_days.replace("일",""));
+        sumWorkTime = rtnJson.attDeptMgrList[0].sum_tot_min;
+        v_sumWorkTime = rtnJson.attDeptMgrList[0].v_tot_min;
+        v_pos_time = rtnJson.attDeptMgrList[0].v_pos_time;
+        pos_time = rtnJson.attDeptMgrList[0].pos_time;
+
+        getAttendDetail();
+        
+      } else {
+        document.querySelector("#_result").innerHTML = 'Error : '+xhr.responseText;
+      }
+    };
+    xhr.open('POST', 'http://gwa.webzen.co.kr/wz/reward/searchRewardAttendEmpList');
+    xhr.setRequestHeader('Content-Type', 'application/json'); 
+    xhr.send(JSON.stringify(data)); 
+  }
+
+  /* 일별 근태현황 조회*/
+  function getAttendDetail(){
+      var xhr = new XMLHttpRequest();
+      var data = {
+        compSeq: _compSeq,
+        deptName: _deptName,
+        empSeq: _empSeq,
+        month: _month,
+        year: _year,
+      };
+      xhr.onload = function() {
+        if (xhr.status === 200 || xhr.status === 201) {
+          parseAttDetailData(xhr.responseText)
+          drawResult();
+        } else {
+          document.querySelector("#_result").innerHTML = '일별 근태 현황 조회 중 오류 발생 : '+xhr.responseText;
+        }
+    
+      };
+      xhr.open('POST', 'http://gwa.webzen.co.kr/wz/reward/searchRewardAttendList');
+      xhr.setRequestHeader('Content-Type', 'application/json'); 
+      xhr.send(JSON.stringify(data)); 
+  }
+  
+  /* 근태 상세 데이터 파싱*/
+  function parseAttDetailData(jsonText){
+    try{
+      var jsonObj = JSON.parse(jsonText);
+      for(var i=0; i <jsonObj.attDeptMgrList.length; i++){
+        
+        if(jsonObj.attDeptMgrList[i].dateMD == "계"){
+          break;
+        }
+        var dayOfList = jsonObj.attDeptMgrList[i].dateMD.replace("월 ","").replace("일","")
+        
+        if(jsonObj.attDeptMgrList[i].tot_min > 0 && jsonObj.attDeptMgrList[i].h_title == ""){
+          if( dayOfList < _today){
+            workedDay++;
+          }else{
+            advanceWorkDay++;
+          }
+        }
+
+        if( dayOfList.toString() == _today.toString()){
+          
+		  if(jsonObj.attDeptMgrList[i].tot_min > 0){
+            basedDay = "오늘";
+          }else{
+            if(jsonObj.attDeptMgrList[i].come_dt != ""){
+              workedToday++;
+              var todayComeTime = jsonObj.attDeptMgrList[i].come_dt;
+              todayWorkTime = convertHourFormatToMin(currentTime) - convertHourFormatToMin(todayComeTime)
+              //점심시간(13시~14시)이 지난경우 60분(중식) 차감
+              if(convertHourFormatToMin(currentTime) > 14*60){
+                todayWorkTime = todayWorkTime - 60;
+              }
+			  
+			  //휴게시간을 체크 한 경우
+			  if(jsonObj.attDeptMgrList[i].tot_min < 0){
+				  todayWorkTime = todayWorkTime + jsonObj.attDeptMgrList[i].tot_min;
+			  }
+			  
+            }
+          }
+        }
+      }
+      //누적 근무 일수
+      totWorkDay = workedDay+advanceWorkDay;
+      //잔여 근무 일수 
+      remWorkDay = base_work_day- totWorkDay
+      //누적 일 평균 근무시간
+      if(sumWorkTime==0)
+          avrWorkTime = 0;
+      else
+          avrWorkTime = Math.round(sumWorkTime/(totWorkDay));
+      //잔여 일 평균 근무시간
+      if(base_workTime - sumWorkTime==0)
+        remAvrWorkTime = 0
+      else 
+		remAvrWorkTime = Math.round((base_workTime - sumWorkTime)/remWorkDay);
+      //초과 근무시간
+      overWorkTime = sumWorkTime - ((totWorkDay) * 480);
+      //overWorkTimeToday = (sumWorkTime + todayWorkTime) - ((totWorkDay + workedToday) * 480)
+	  if(totWorkDay < base_work_day)
+		overWorkTimeToday = (sumWorkTime + todayWorkTime) - ((totWorkDay + workedToday) * 480)
+	  else
+		overWorkTimeToday = (sumWorkTime + todayWorkTime) - ( base_work_day * 480) 
+	  //오늘 퇴근 시간
+	  if(overWorkTimeToday < 0){
+		  //점심시간(13시~14시)이 지나지 않은 경우 60분(중식) 계산
+		  var remainWorktime = overWorkTimeToday;
+		  if(convertHourFormatToMin(currentTime) <= 13*60){
+			remainWorktime = remainWorktime - 60;
+		  }
+			  
+		today.setMinutes(today.getMinutes()+Math.abs(remainWorktime));
+		todayLeftHour = "data-toggle='tooltip' data-placement='top' title=' 오늘 "+ new String(today.getHours()).padStart(2, '0')+":"+new String(today.getMinutes()).padStart(2, '0') +" 퇴근 시 플러스 전환'";
+	  }
+	  
+    }catch(e){
+          showError(e.message);
+    }
+  }
+  
+  /* 계산 결과 표시 */
+  function drawResult(){
+    message = "<table id='summary' width='100%'>"+
+			"	<tr>"+
+			"		<th width='50%'>인정 근무 일수</th>"+
+			"		<th width='50%'>인정 근무 시간</th>"+
+			"	</tr>"+
+			"	<tr>"+
+			"		<td>"+workedDay+"일";
+			if(advanceWorkDay >0){
+				message += "("+advanceWorkDay+"일)";
+			}
+message += 	" / "+base_work_day+"일</td>"+
+			"		<td>"+v_sumWorkTime+"</td>"+
+
+      
+            "	</tr>"+
+            "	<tr>"+
+            "		<th>진행 평균 근무</th>"+
+            "		<th>잔여 평균 근무</th>"+
+            "	</tr>"+
+            "	<tr>"+
+            "		<td>"+convertMinToHourFormat(avrWorkTime)+"</td>"+
+            "		<td>"+convertMinToHourFormat(remAvrWorkTime)+"</td>"+
+            "	</tr>"+
+            "	<tr>"+
+            "		<th>초과 시간(" + basedDay + "기준)</th>"+
+            "		<th>초과 시간(현재기준)</th>"+
+            "	</tr>"+
+            "	<tr>"+
+            "		<td class='"+ (overWorkTime < 0 ? "alert-danger":"alert-success") +"'>"+convertMinToHourFormat(overWorkTime)+"</td>"+
+            "		<td class='"+ (overWorkTimeToday < 0 ? "alert-danger":"alert-success") +"' "+todayLeftHour+">"+convertMinToHourFormat(overWorkTimeToday)+"</td>"+
+            "	</tr>"+
+            "</table>";
+
+      document.querySelector("#_result").innerHTML = message;
+      document.querySelector("#_current_time").innerText = longCurrentTime;
+  }
+  
+  function showError(message){
+    document.querySelector("#_result").innerHTML = "Error<br>"+message;
+  }
+    
+});
  
